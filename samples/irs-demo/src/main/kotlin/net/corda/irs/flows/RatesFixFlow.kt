9123f5ecc23e7ea4243460d0666993ff82c60352
Ross Nicoll <ross.nicoll@r3.com>
2017-07-31 18:42:15 +0100

    Start removing addSignatureUnchecked

--- samples/irs-demo/src/main/kotlin/net/corda/irs/flows/RatesFixFlow.kt
+++ samples/irs-demo/src/main/kotlin/net/corda/irs/flows/RatesFixFlow.kt
@@ -64,7 +64,7 @@ open class RatesFixFlow(protected val tx: TransactionBuilder,
         progressTracker.currentStep = SIGNING
         val mtx = tx.toWireTransaction().buildFilteredTransaction(Predicate { filtering(it) })
         val signature = subFlow(FixSignFlow(tx, oracle, mtx))
-        tx.addSignatureUnchecked(signature)
+        tx.signInitialTransaction(signature)
     }
     // DOCEND 2
 
